0947da882610e02d5c97357d6fbd4e01229b81d2
aman.arnold <aman.arnold@yahoo.com>
2017-06-11 00:23:39 +0100

    fixed print call using spaces instead of tabs

--- 01_blinkingLed.py
+++ 01_blinkingLed.py
@@ -1,14 +1,5 @@
 #!/usr/bin/env python
 
-#-----------------------------------------------------------
-# File name   : 01_blinkingLed_1.py
-# Description : make an led blinking.
-# Author      : Jason
-# E-mail      : jason@adeept.com
-# Website     : www.adeept.com
-# Date        : 2015/06/12
-#-----------------------------------------------------------
-
 import RPi.GPIO as GPIO
 import time
 
@@ -19,13 +10,13 @@ GPIO.setup(LedPin, GPIO.OUT)   # Set pin mode as output
 GPIO.output(LedPin, GPIO.HIGH) # Set pin to high(+3.3V) to off the led
 
 try:
-	while True:
-		print '...led on'
-		GPIO.output(LedPin, GPIO.LOW)  # led on
-		time.sleep(0.5)
-		print 'led off...'
-		GPIO.output(LedPin, GPIO.HIGH) # led off
-		time.sleep(0.5)
+    while True:
+        print('...led on')
+        GPIO.output(LedPin, GPIO.LOW)  # led on
+        time.sleep(0.5)
+        print('led off...')
+        GPIO.output(LedPin, GPIO.HIGH) # led off
+        time.sleep(0.5)
 except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the flowing code will be  executed.
-	GPIO.output(LedPin, GPIO.HIGH)     # led off
-	GPIO.cleanup()                     # Release resource
+    GPIO.output(LedPin, GPIO.HIGH)     # led off
+    # GPIO.cleanup()                     # Release resource
